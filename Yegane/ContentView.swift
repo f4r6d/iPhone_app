3fa66c2521ebf8e85fe3499b9158825ac9fd9f35
farshidelf <farshidelf@yahoo.com>
2023-04-13 02:22:46 +0330

    Initial Commit

---
+++ Yegane/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Yegane
+//
+//  Created by Farshid Gholizadeh on 4/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yegane/YeganeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeganeApp.swift
+//  Yegane
+//
+//  Created by Farshid Gholizadeh on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct YeganeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
